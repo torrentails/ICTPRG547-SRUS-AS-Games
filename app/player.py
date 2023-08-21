7bc1b59770f6ee9ad411eb481062820e192e4e7c
Alex Sullivan <torrentails@torrentails.com>
2023-08-21 11:40:05 +0800

    Adds add_password to Player

--- app/player.py
+++ app/player.py
@@ -1,3 +1,7 @@
+import random
+from argon2 import PasswordHasher
+
+
 class Player:
     """Represents a player with a name and an id
 
@@ -13,6 +17,8 @@ class Player:
 
     _uid: str
     _name: str
+    _password_salt: bytes
+    _password_hash: str | None
 
     def __init__(self, uid: str, name: str):
         """
@@ -25,6 +31,8 @@ class Player:
         """
         self._uid = uid
         self._name = name
+        self._password_hash = None
+        self._password_salt = b''.join([random.randbytes(1) for _ in range(8)])
 
     @property
     def uid(self) -> str:
@@ -33,6 +41,25 @@ class Player:
     @property
     def name(self) -> str:
         return self._name
+
+    def add_password(self, password: str) -> None:
+        """Generates and stores a password hash with the given password
+
+        Parameters
+        ----------
+        password
+            the password to hash
+
+        Raises
+        ------
+        RuntimeError
+            if a password is already set
+        """
+        if self._password_hash is not None:
+            raise RuntimeError("Password already set")
+
+        hasher = PasswordHasher()
+        self._password_hash = hasher.hash(password, salt=self._password_salt)
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}({self.uid}, {self.name})"
