063f7d3e8d86770a71070bab0c5b50b5f9b79239
Alex Sullivan <torrentails@torrentails.com>
2023-08-21 11:49:23 +0800

    Adds tests for Player passwords

--- test/player_test.py
+++ test/player_test.py
@@ -7,14 +7,25 @@ class PlayerTest(unittest.TestCase):
     def setUp(self) -> None:
         self.player_uid = '357'
         self.player_name = 'Casey'
+        self.password = 'HelloWorld-1'
         self.player = Player(self.player_uid, self.player_name)
 
     def test_properties(self):
-
         self.assertEqual(self.player.uid, self.player_uid,
                          f"player.uid is not {self.player_uid}")
         self.assertEqual(self.player.name, self.player_name,
                          f"player.name is not {self.player_name}")
+
+    def test_password(self):
+        self.assertRaises(RuntimeError, self.player.verify_password, self.password)
+        self.assertRaises(RuntimeError, self.player.verify_password, "SomePass!9")
+
+        self.player.add_password(self.password)
+        self.assertRaises(RuntimeError, self.player.add_password, "SomeOtherPass456")
+        self.assertRaises(RuntimeError, self.player.add_password, self.password)
+
+        self.assertTrue(self.player.verify_password(self.password))
+        self.assertFalse(self.player.verify_password("a_failing_pass"))
 
 
 if __name__ == '__main__':
